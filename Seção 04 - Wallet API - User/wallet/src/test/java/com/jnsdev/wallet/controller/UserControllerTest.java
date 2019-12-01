1574780f91abd9f3aeae074b2020d0be6a20e6f7
jaironsousa1 <jaironsousa@gmail.com>
2019-12-01 17:39:59 -0300

    Seção 4 - 18. UserController

---
+++ Seção 04 - Wallet API - User/wallet/src/test/java/com/jnsdev/wallet/controller/UserControllerTest.java
@@ -0,0 +1,74 @@
+package com.jnsdev.wallet.controller;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.net.URI;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jnsdev.wallet.entity.User;
+import com.jnsdev.wallet.service.UserService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserControllerTest {
+
+	private static final String EMAIL = "email@teste.com";
+
+	private static final String NAME = "User teste";
+
+	private static final String PASSWORD = "123456";
+
+	private static final String URL = "/user";
+
+	/**
+	 * Vamos simular requisições na rota de usuario
+	 */
+	
+	@MockBean
+	UserService service;
+	
+	@Autowired
+	MockMvc mvc;
+	
+	public void testSave() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.post(URL)
+				.content(getJsonPayload())
+				.contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
+		.andExpect(status().isCreated());
+	}
+	
+	public User getMockUser() {
+		User u = new User();
+		u.setEmail(EMAIL);
+		u.setName(NAME);
+		u.setPassowrd(PASSWORD);
+		
+		return u;
+	}
+	
+	public String getJsonPayload() {
+		UserDTO dto = new UserDTO();
+		dto.setEmail(EMAIL);
+		dto.setName(NAME);
+		dto.setPassowrd(PASSWORD);
+		
+		ObjectMapper mapper = new ObjectMapper();
+
+		return mapper.writeValueAsString(dto);
+		
+	}
+}
