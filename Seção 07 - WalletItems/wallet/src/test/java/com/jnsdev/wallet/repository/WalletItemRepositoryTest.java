0544e0a128688ae9d64b42e023fdd57c3f05c55e
Jairo Nascimento <jaironsousa@gmail.com>
2020-01-21 14:09:35 -0300

    Seção 7: 39 - WalçletItemsRepositoryTest

---
+++ Seção 07 - WalletItems/wallet/src/test/java/com/jnsdev/wallet/repository/WalletItemRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.jnsdev.wallet.repository;
+
+import com.jnsdev.wallet.entity.Wallet;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class WalletItemRepositoryTest {
+
+    private static final Date DATE = new Date();
+    private static final String TYPT = "ENTRADA";
+    private static final String DESCRIPTION = "Conta de Luz";
+    private static final BigDecimal VALUE = BigDecimal.valueOf(65);
+
+    @Autowired
+    WalletItemRepository repository;
+
+    @Test
+    public void testSave(){
+        Wallet w = new Wallet();
+        w.setName("Carteira 1");
+        w.setValue(BigDecimal.valueOf(500));
+
+        WalletItem wi = WalletItem(w, DATE, TYPT, DESCRIPTION, VALUE);
+
+        WalletItem response = repository.save(wi);
+
+        Assert.assertNotNull(response);
+    }
+}
