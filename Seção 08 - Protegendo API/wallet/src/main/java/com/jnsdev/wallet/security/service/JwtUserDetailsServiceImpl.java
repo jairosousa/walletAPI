107e3c533dfc695a14cf3ac032922e3806a2d822
Jairo Nascimento <jaironsousa@gmail.com>
2020-02-03 16:18:03 -0300

    Seção 8: 53. Configurações de segurança

---
+++ Seção 08 - Protegendo API/wallet/src/main/java/com/jnsdev/wallet/security/service/JwtUserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.jnsdev.wallet.security.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.jnsdev.wallet.entity.User;
+import com.jnsdev.wallet.security.JwtUserFactory;
+import com.jnsdev.wallet.service.UserService;
+
+public class JwtUserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserService userService;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<User> user = userService.findByEmail(email);
+
+		if (user.isPresent()) {
+			return JwtUserFactory.create(user.get());
+		}
+
+		throw new UsernameNotFoundException("Email não encontrado.");
+	}
+
+}
